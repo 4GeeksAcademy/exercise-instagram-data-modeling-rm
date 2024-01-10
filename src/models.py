688cf6c5aa6b0ca337a6113abfac54f60cb407df
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2024-01-10 00:00:19 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -7,22 +7,22 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(50), nullable=False)
-    password = Column(String(200))
-    
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     user_mail = Column(String(50), unique=True, nullable=False)
-    user_id = Column(Integer, ForeignKey('follower.id'))
-    user = relationship(Follower)
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(50), nullable=False)
+    password = Column(String(200))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    
 class Posts(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
@@ -30,8 +30,8 @@ class Posts(Base):
     comment_id = Column(String(700))
     likes_id = Column(Integer)
     photos_id = Column(Integer)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('follower.id'))
+    user = relationship(Follower)
 
 class Photos(Base):
     __tablename__ = 'photos'
@@ -39,16 +39,16 @@ class Photos(Base):
     users_id = Column(String)
     description_id = Column(String(250))
     likes_id = Column(Integer)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('follower.id'))
+    user = relationship(Follower)
 
 class Comments(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     users_id = Column(String)
     comment = Column(String(250))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('follower.id'))
+    user = relationship(Follower)
 
     def to_dict(self):
         return {}
